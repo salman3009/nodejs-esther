6be2e53914615c606c366afd27851a60f2ede899
salman3009 <salman444g@gmail.com>
2023-05-20 11:36:27 +0530

    code

--- Course/May/20-05-23/basic-express/app.js
+++ Course/May/20-05-23/basic-express/app.js
@@ -3,6 +3,11 @@ const app = express();
 const PORT = 3000;
 
 
+app.get('',(req,res)=>{
+  res.send("<h1>Welcome to express</h1>");
+})
+
+
 app.listen(PORT,()=>{
     console.log("server is running on 3000");
 })
