090773fc4bd13cd8ddea3c6a7260e400bdf901ad
salman3009 <salman444g@gmail.com>
2023-05-21 09:41:57 +0530

    code

---
+++ Course/May/21-05-23/express-get-post/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('',(req,res)=>{
+  res.status(200).json({message:"fetched successfully"});
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("server is running on 3000");
+})
