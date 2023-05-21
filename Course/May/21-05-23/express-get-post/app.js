74f7a1920f01a1e6b28c45ee707eccb2225c1744
salman3009 <salman444g@gmail.com>
2023-05-21 09:57:45 +0530

    code

--- Course/May/21-05-23/express-get-post/app.js
+++ Course/May/21-05-23/express-get-post/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+//http://localhost:3000
 app.get('',(req,res)=>{
   res.status(200).json({message:"fetched successfully"});
 })
 
+//http://localhost:3000
+app.post('',(req,res)=>{
+    console.log("inside the post method");
+     res.status(201).json({message:"Data is created successfully"});
+})
 
 
 app.listen(3000,()=>{
