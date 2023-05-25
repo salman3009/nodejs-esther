20b5056cc901457be011c10dbd47cd166a4b3cf0
salman3009 <salman444g@gmail.com>
2023-05-25 22:21:34 +0530

    code

--- Course/May/24-05-23-tour-crud-operation-authorization-params-query-params/tour-api/app.js
+++ Course/May/24-05-23-tour-crud-operation-authorization-params-query-params/tour-api/app.js
@@ -28,6 +28,7 @@ app.post('/register',(req,res)=>{
 })
 
 const loginValidationMiddleware=(req,res,next)=>{
+    //authenticate process
     console.log("coming to loginValidationMiddleware");
     if(!req.body.email || !req.body.password){
         return res.sendFile(__dirname+"/error.html");
@@ -36,6 +37,7 @@ const loginValidationMiddleware=(req,res,next)=>{
 }
 
 const loginCheckingDetailsMiddleware=(req,res,next)=>{
+  //authenticate process
     console.log("checking email and password data");
     if(req.body.email != user.email || req.body.password != user.password){
         return res.sendFile(__dirname + '/error.html');
@@ -45,6 +47,7 @@ const loginCheckingDetailsMiddleware=(req,res,next)=>{
 
 const checkLoginStatus=(req,res,next)=>{
     console.log(user);
+    //authorization process
     if(user.loginStatus && user.role === 'admin'){
        return next();
     }
@@ -62,12 +65,15 @@ app.post('/login',loginValidationMiddleware,loginCheckingDetailsMiddleware,(req,
 
 app.post('/tourcreate',checkLoginStatus,(req,res)=>{
     console.log("everything is created successfully");
+    list.push(req.body);
     res.sendFile(__dirname+'/dashboard.html');
 })
 
 app.get('/tourread',(req,res)=>{
     console.log("everything is created successfully");
-    res.send('<h1>reading data</h1>');
+    res.status(200).json({
+        data:list
+    });
 })
 
 
