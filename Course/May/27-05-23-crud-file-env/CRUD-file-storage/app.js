3672ff0df966a14aa5b3ccaa94287048917a02ba
salman3009 <salman444g@gmail.com>
2023-05-27 10:52:38 +0530

    code

--- Course/May/27-05-23-crud-file-env/CRUD-file-storage/app.js
+++ Course/May/27-05-23-crud-file-env/CRUD-file-storage/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const env = require('dotenv');
+const exp = require('constants');
 env.config();
 
+app.use(express.json());
 
 const list = JSON.parse(fs.readFileSync(`${__dirname}/data/tour.json`));
 console.log(list);
@@ -12,6 +14,16 @@ app.get('',(req,res)=>{
     res.send(list);
 });
 
+app.post('',(req,res)=>{
+   let product = req.body;
+   product.id = list.length+1;
+   list.push(product);
+   fs.writeFile(`${__dirname}/data/tour.json`,JSON.stringify(list),()=>{
+      res.status(201).send("<h1>Data added successfully");
+   })
+
+})
+
 app.listen(process.env.PORT,()=>{
-    console.log("server is running on",process.env.PORT);
+    console.log("server is running xon",process.env.PORT);
 })
