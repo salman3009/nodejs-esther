06f6955d9b719dceca1d857669513838813752a9
salman3009 <salman444g@gmail.com>
2023-05-28 11:09:59 +0530

    code

--- Course/May/28-05-23-express-routing/routing/app.js
+++ Course/May/28-05-23-express-routing/routing/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const userRoute = require('./routes/user');
+const path = require('path');
 const env = require('dotenv');
 env.config();
 
+
+app.use('',express.static(path.join(__dirname,'views')));
 app.use('/api/user',userRoute);
 
 app.listen(process.env.PORT,()=>{
