b81bf42a1a9229088f6997a9daaf1fc12ff94751
salman3009 <salman444g@gmail.com>
2023-05-30 22:36:44 +0530

    code

--- Course/May/30-05-23-express-read-file-mvc/routing/routes/product.js
+++ Course/May/30-05-23-express-read-file-mvc/routing/routes/product.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controller/product');
+const validationMiddleware = require('../middlewares/validation');
 
 
 router.get('',productController.getListController);
-router.post('',productController.postListController);
+router.post('',validationMiddleware,productController.postListController);
 
 module.exports = router;
