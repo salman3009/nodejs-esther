a0563255e4a20ceceb0c613aba67398c21d027d6
salman3009 <salman444g@gmail.com>
2023-06-15 22:48:04 +0530

    code

--- Course/June/15-06-23-Mongdb_atlas_find_update_delete/crud_mongoose/app.js
+++ Course/June/15-06-23-Mongdb_atlas_find_update_delete/crud_mongoose/app.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose');
 const uri = "mongodb://localhost:27017/banking";
+const Customer = require('./models/customer');
 
 mongoose.connect(uri).then(()=>{
     console.log("database is connected");
+    createOperation();
 }).catch((err)=>{
     console.log("connection failed",err)
 })
+
+const createOperation=async()=>{
+    try{
+
+        const customerPost = new Customer({
+            fullName:"akash",
+            age:45,
+            status:false,
+            hobbies:['football','music'],
+            salary:15000
+        })
+
+        const result = await customerPost.save();
+        console.log(result);
+
+    }catch(err){
+        console.log(err);
+    }
+}
