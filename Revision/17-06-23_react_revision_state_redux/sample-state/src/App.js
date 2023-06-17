d27aca297ef3dc1193555e1cbac5bf05b7ec17f2
salman3009 <salman444g@gmail.com>
2023-06-17 10:55:46 +0530

    code

--- Revision/17-06-23_react_revision_state_redux/sample-state/src/App.js
+++ Revision/17-06-23_react_revision_state_redux/sample-state/src/App.js
@@ -1,37 +1,13 @@
-import {useState} from 'react';
-// function App(){
-
-//       let fullName = "suresh";
-
-//       const onchangeHandler=(event)=>{
-//         fullName = event.target.value;
-//         console.log(fullName);
-//       }
-
-//       return (
-//         <>
-//         <div>{fullName}</div>
-//         <div><input type="text" onChange={onchangeHandler}/></div>
-//         </>
-//       )
-// }
-// export default App;
-
-
-
+import StateUpdateWithoutHooks from "./StateUpdateWithoutHooks";
+import UseState from "./UseState";
+import UseReducer from "./UseReducer";
 
 function App(){
-
-   const [getState,setState]=useState('suresh');
-
-  const onchangeHandler=(event)=>{
-    setState(event.target.value)
-  }
-
   return (
     <>
-    <div>{getState}</div>
-    <div><input type="text" onChange={(event)=>setState(event.target.value)}/></div>
+   {/* <StateUpdateWithoutHooks/> */}
+   {/* <UseState/> */}
+   <UseReducer/>
     </>
   )
 }
