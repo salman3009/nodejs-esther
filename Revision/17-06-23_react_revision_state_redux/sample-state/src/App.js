ece2a907dc5d8f46170108517449a00a7e317fbc
salman3009 <salman444g@gmail.com>
2023-06-17 11:28:21 +0530

    code

--- Revision/17-06-23_react_revision_state_redux/sample-state/src/App.js
+++ Revision/17-06-23_react_revision_state_redux/sample-state/src/App.js
@@ -1,13 +1,26 @@
 import StateUpdateWithoutHooks from "./StateUpdateWithoutHooks";
 import UseState from "./UseState";
 import UseReducer from "./UseReducer";
+import Display from "./Display";
+import { useState } from "react";
+import React from "react";
+
+export const UserContext = React.createContext();
+
 
 function App(){
+
+  const [getCity,setCity] = useState('Delhi');
+
   return (
     <>
    {/* <StateUpdateWithoutHooks/> */}
    {/* <UseState/> */}
-   <UseReducer/>
+   {/* <UseReducer/> */}
+   <UserContext.Provider value={getCity}> 
+   <Display city={getCity}/>
+   </UserContext.Provider>
+  
     </>
   )
 }
