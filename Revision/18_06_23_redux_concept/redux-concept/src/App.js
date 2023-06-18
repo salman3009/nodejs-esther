45bf369120b2017cfe1d7877946ef31b71551ad1
salman3009 <salman444g@gmail.com>
2023-06-18 10:59:16 +0530

    code

--- Revision/18_06_23_redux_concept/redux-concept/src/App.js
+++ Revision/18_06_23_redux_concept/redux-concept/src/App.js
@@ -1,16 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import './App.css';
-
+import { increment,decrement,amount } from './counterSlice';
 
 function App() {
     
   //const value = useSelector((state)=>state.counter.value);
   const {value,email} = useSelector((state)=>state.counter);
+  const dispatch = useDispatch();
+
 
   return (
     <div className="App">
       <h1>{value}-{email}</h1>
        <h1>Hello Redux</h1>
+        <br/>
+        <button onClick={()=>dispatch(increment())}>Increment</button>
     </div>
   );
 }
