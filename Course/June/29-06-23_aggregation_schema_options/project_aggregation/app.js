8fe144fa81e1a2ceb1f3bfcba952bc109ff51ea0
salman3009 <salman444g@gmail.com>
2023-06-29 21:47:11 +0530

    code

--- Course/June/29-06-23_aggregation_schema_options/project_aggregation/app.js
+++ Course/June/29-06-23_aggregation_schema_options/project_aggregation/app.js
@@ -5,7 +5,24 @@ const uri = "mongodb://localhost:27017/aggregation";
 
 mongoose.connect(uri).then(()=>{
     console.log("database is connected");
+    createOperation();
 }).catch(()=>{
     console.log("database is failed");
 })
+
+const createOperation=async()=>{
+    try{
+        const employeePost = new Employee({
+            firstName:'ganesh',
+            age:22,
+            status:true,
+            hobbies:['swimming'],
+            salary:18000
+        })
+        const result = await employeePost.save();
+        console.log(result);
+    }catch(err){
+        console.log(err);
+    }
+}
 
