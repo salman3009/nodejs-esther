6ac38fd8da5c06508114b836f62730c08cba5594
salman3009 <salman444g@gmail.com>
2023-07-01 11:24:28 +0530

    code

---
+++ Course/July/01-07-23-schema-options-virtual-properties/hooks/models/employee.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+const employeeSchema = mongoose.Schema({
+    firstName:{type:String,required:true},
+    lastName:{type:String,required:true}
+})
+
+
+
+
+
+module.exports = mongoose.model('employee',employeeSchema);
