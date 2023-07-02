8a9c503c4d9b80c1526aafc3b06f3c68fef2f4bb
salman3009 <salman444g@gmail.com>
2023-07-02 10:55:05 +0530

    code

--- Assignment/15-june-23/Count-the-Number-of-Products-Level-1/src/app.js
+++ Assignment/15-june-23/Count-the-Number-of-Products-Level-1/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 var products   =require("../models/product.js");
+const { parse } = require('dotenv');
 
 // Import routes
 
@@ -26,7 +27,55 @@ app.use(express.json());
 
 app.get("/",async function(req,res){
 
+    let category = req.query.category;
+    let p_range = req.query.range;
+
     var count = 0;
+    let m_price = 0;
+    let mx_price = 100000000;
+
+    if(p_range){
+        m_price = '';
+        mx_price = '';
+        let f = 0;
+        "5000"
+        for(let i=0;i<p_range.length;i++){
+            if(p_range[i] == '-'){
+                f=1;
+                continue;
+            }
+            if(f){
+                mx_price = mx_price + p_range[i];
+            }else{
+                m_price = m_price + p_range[i];
+            }
+        }
+        m_price = parseInt(m_price);
+        console.log(m_price);
+        if(mx_price.length){
+            mx_price = parseInt(mx_price);
+        }
+        else {
+            mx_price = 1000000;
+        }
+    }
+
+    let result = await products.find();
+    
+    for(let i=0;i<result.length; i++){
+        if(result[i]['price']>=m_price){
+            if(result[i]['price']<=mx_price){
+                if(category){
+                    if(category == result[i]['category']){
+                        count++;
+                    }   
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+    }
 
     //Write you code here
     //update count variable 
