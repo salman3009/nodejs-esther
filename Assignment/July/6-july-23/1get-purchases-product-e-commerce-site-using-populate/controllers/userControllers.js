c8e5fe7b34ed5203b0d61f419c273c4a9b1452d2
salman3009 <salman444g@gmail.com>
2023-07-09 11:45:16 +0530

    code

--- Assignment/July/6-july-23/1get-purchases-product-e-commerce-site-using-populate/controllers/userControllers.js
+++ Assignment/July/6-july-23/1get-purchases-product-e-commerce-site-using-populate/controllers/userControllers.js
@@ -59,7 +59,7 @@ const getProductsPurchasedByUser = async (req, res) => {
             message:'Products Purchased by User',
             status: "success",
             data:{
-                products:user
+                products:user.productsPurchased
             }
 
         })
