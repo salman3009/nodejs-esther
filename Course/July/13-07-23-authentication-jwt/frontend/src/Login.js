7ec05cfda3972876f3de62b2562177b52615d77c
salman3009 <salman444g@gmail.com>
2023-07-13 22:58:23 +0530

    code

--- Course/July/13-07-23-authentication-jwt/frontend/src/Login.js
+++ Course/July/13-07-23-authentication-jwt/frontend/src/Login.js
@@ -8,26 +8,33 @@ const Login = () => {
          password:''
     })
 
+    const [getError,setError] = useState('')
+
     const onChangeHandler=(event)=>{
         setData({...getData,[event.target.name]:event.target.value})
     }
 
     const onSubmitHandler=(event)=>{
          event.preventDefault();
-         if(!getData.userName || !getData.email || !getData.password){
+         if(!getData.email || !getData.password){
              alert('please provide the details');
              return ;
          }
-         axios.post('http://localhost:8080/api/user/register',getData).then(()=>{
+         axios.post('http://localhost:8080/api/user/login',getData).then(()=>{
             alert("successful");
-         }).catch(()=>{
-            alert("internal server error");
+            setError('');
+         }).catch((error)=>{
+            if(error && error.response && error.response.data){
+                setError(error.response.data.message)
+            }
+            
          })
     }
 
     return (<div className="container">
         <div className="col-4"></div>
         <div className="col-4">
+           {getError && <h1 style={{color:'red'}}>{getError}</h1>} 
             <form>
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
