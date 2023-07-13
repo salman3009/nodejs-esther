2572067ba5e97ae56f971c7e99f529ae8c2e3a09
salman3009 <salman444g@gmail.com>
2023-07-13 21:35:35 +0530

    code

---
+++ Course/July/13-07-23-authentication-jwt/backend/controllers/user.js
@@ -0,0 +1,30 @@
+const User = require('../models/User');
+const bcrypt = require(bcrypt);
+
+const register = async(req,res)=>{
+  try{
+    const {userName,email,password} = req.body;
+
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password,salt);
+
+    const user = new User({
+        userName,
+        email,
+        password:hashedPassword
+    })
+
+    const result = await user.save();
+    res.status(201).json({
+        user:{result}
+    })
+
+  }catch(err){
+    return res.status(400).json({
+        message:err
+    })
+  }
+}
+
+module.exports.register = register;
