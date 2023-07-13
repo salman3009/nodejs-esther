83a7b72cd58f4d25b228c546bcd0680e12f6354c
salman3009 <salman444g@gmail.com>
2023-07-13 22:39:38 +0530

    code

--- Course/July/13-07-23-authentication-jwt/frontend/src/App.js
+++ Course/July/13-07-23-authentication-jwt/frontend/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Header from './Header';
 import Register from './Register';
+import Login from './Login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Register/>
+      <BrowserRouter>
+       <Header/>
+       <Routes>
+       <Route path='/' element={<Register/>}/>
+       <Route path='/login' element={<Login/>}/>
+       </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
