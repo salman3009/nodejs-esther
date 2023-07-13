e7dd70030046fa6b4f0fff64b525e96ba31f775c
salman3009 <salman444g@gmail.com>
2023-07-13 22:13:01 +0530

    code

---
+++ Course/July/13-07-23-authentication-jwt/frontend/src/Register.js
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+const Register = () => {
+
+    const [getData,setData] = useState({
+         userName:'',
+         email:'',
+         password:''
+    })
+
+    const onChangeHandler=(event)=>{
+        setData({...getData,[event.target.name]:event.target.value})
+    }
+
+    const onSubmitHandler=(event)=>{
+         event.preventDefault();
+         if(!getData.userName || !getData.email || !getData.password){
+             alert('please provide the details');
+             return ;
+         }
+         console.log(getData);
+    }
+
+    return (<div className="container">
+        <div className="col-4"></div>
+        <div className="col-4">
+            <form>
+
+                <div className="form-group">
+                    <label htmlFor="userName">User Name</label>
+                    <input type="text" className="form-control" onChange={onChangeHandler} name="userName" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" className="form-control" onChange={onChangeHandler} name="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" onChange={onChangeHandler} name="password" />
+                </div>
+
+                <button type="submit" onClick={onSubmitHandler} className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div className="col-4"></div>
+
+    </div>)
+}
+
+export default Register;
